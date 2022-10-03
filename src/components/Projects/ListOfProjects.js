d24d182153fc565f918fe5004626536e1ccd817b
Sebastian Młotkowski <sebamlot142@gmail.com>
2022-10-03 23:18:59 +0200

    Added new projects

--- src/components/Projects/ListOfProjects.js
+++ src/components/Projects/ListOfProjects.js
@@ -7,11 +7,25 @@ import portfolio from './Images/MagdaPortfolio.png';
 
 export const projects = [
   {
+    name: 'E-mail App',
+    comment: 'Small E-mail app, which uses mongoDB to store data from the user.',
+    imageURL: email,
+    href: 'https://task5sebastianmlotkowski.herokuapp.com',
+    id: 1,
+  },
+  {
+    name: 'Random User',
+    comment: 'Random user from chosen country with randomly placed errors generator. Also CSV functionality',
+    imageURL: userrandom,
+    href: 'https://github.com/Mundsen96/Tenzies',
+    id: 2,
+  },
+  {
     name: 'Tenzies',
     comment: 'Tenzies Game',
     imageURL: tenzies,
-    href: 'https://github.com/Mundsen96/Tenzies',
-    id: 1,
+    href: 'https://task6sebastianmlotkowski.netlify.app',
+    id: 3,
   },
   {
     name: 'Quiz Game',
@@ -19,28 +33,28 @@ export const projects = [
       'A Quiz-game, created in React, which takes random questions from API, based on the custom attributes, taken by form on first screen. The API addres is https://opentdb.com/api_config.php.',
     imageURL: quizGame,
     href: 'https://github.com/Mundsen96/Quiz-Game',
-    id: 2,
+    id: 4,
   },
   {
     name: 'Meme Generator',
     comment: 'Meme Generator, which takes photos from API',
     imageURL: meme_generator,
     href: 'https://github.com/Mundsen96/Meme-Generator',
-    id: 3,
+    id: 5,
   },
   {
     name: 'Airbnb Copy',
     comment: 'Copy of Airbnb page',
     imageURL: airbnb,
     href: 'https://github.com/Mundsen96/airbnb',
-    id: 4,
+    id: 6,
   },
   {
     name: 'Travel Journal',
     comment: 'Simple Travel Journal',
     imageURL: travel,
     href: 'https://github.com/Mundsen96/travel-journal-React',
-    id: 5,
+    id: 7,
   },
   {
     name: 'Magda Portfolio',
